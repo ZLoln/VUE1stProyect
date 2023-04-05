6a7860316a20c1d89292a7a2037a707a3dc2cf8e
ZLoln <lolanavalo@gmail.com>
2023-04-05 23:39:25 +0200

    Pokemon and Chuck Norris Api connected

---
+++ src/views/ApiPokemonView.vue
@@ -0,0 +1,16 @@
+
+
+<script>
+    import ApiChuckNorris from '../components/myComponents/apiComponent/ApiChuckNorris.vue';
+    import ApiPokemon from '../components/myComponents/apiComponent/ApiPokemon.vue';
+    export default { components: { ApiPokemon, ApiChuckNorris } };
+</script>
+
+<template>
+    
+    <ApiPokemon/>
+    <ApiChuckNorris/>
+    
+</template>
+
+<style></style>
